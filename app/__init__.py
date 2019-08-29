19b1599869f74aee3c943a61c4fa3c8a7ec40bc9
Shiprasahu <siprasahu18@gmail.com>
2019-08-29 17:08:21 +0530

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
  
+#from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
  
 app.config.from_object(Config)
